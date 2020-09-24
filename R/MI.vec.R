43952cf14710fda2a6fda2fd13eb35a57e17195d
sjuhl <35657945+sjuhl@users.noreply.github.com>
2020-09-24 09:42:27 +0200

    update manual

--- R/MI.vec.R
+++ R/MI.vec.R
@@ -2,7 +2,7 @@
 #'
 #' @description Tests for the presence of spatial autocorrelation
 #' in a vector as indicated by the Moran coefficient. The variance
-#' is calculated under assumed normality.
+#' is calculated under the normality assumtion.
 #'
 #' @param x a vector
 #' @param W spatial connectivity matrix
@@ -21,7 +21,14 @@
 #'
 #' @author Sebastian Juhl
 #'
-#' @references Tiefelsdorf, Michael and Barry Boots (1995): The Exact Distribution
+#' @references Cliff, Andrew D. and John K. Ord (1981): Spatial Processes:
+#' Models & Applications. Pion.
+#'
+#' Bivand, Roger S. and David W. S. Wong (2018): Comparing
+#' Implementations of Global and Local Indicators of Spatial Association.
+#' TEST 27: pp. 716 - 748.
+#'
+#' Tiefelsdorf, Michael and Barry Boots (1995): The Exact Distribution
 #' of Moran's I. Environment and Planning A: Economy and Space, 27 (6):
 #' pp. 985 - 999.
 #'
@@ -45,7 +52,8 @@ MI.vec <- function(x,W,alternative="greater"){
   if(any(class(W)!="matrix")) W <- as.matrix(W)
   if(anyNA(x) | anyNA(W)) stop("Missing values detected")
   if(!(alternative %in% c("greater","lower", "two.sided"))){
-    stop("Invalid input: 'alternative' must be either 'greater', 'lower', or 'two.sided'")
+    stop("Invalid input: 'alternative' must be either 'greater',
+         'lower', or 'two.sided'")
   }
 
   #####
@@ -54,10 +62,9 @@ MI.vec <- function(x,W,alternative="greater"){
   #####
   nx <- ncol(x)
   n <- nrow(W)
-  df <- n-1 # 1 because only one x tested at a time
+  df <- n-1 # only one variable considered
   S0 <- t(rep(1,n)) %*% W %*% rep(1,n)
-  #S1 <- .5 * sum((W+W)^2)
-  S1 <- sum((W*W)+(W*t(W))) # similar to moran.test
+  S1 <- sum((W*W)+(W*t(W)))
   S2 <- sum((rowSums(W)+colSums(W))^2)
 
   # projection matrix M
@@ -67,7 +74,6 @@ MI.vec <- function(x,W,alternative="greater"){
   #####
   # Output
   #####
-  # see Tiefelsdorf/ Boots 1995: p. 987 and Murakami et al. 2017: p. 71
   out <- data.frame(matrix(NA,nrow=nx,ncol=6))
   colnames(out) <- c("I","EI","VarI","zI","pI","")
   for(i in 1:nx){
@@ -75,7 +81,7 @@ MI.vec <- function(x,W,alternative="greater"){
     out[i,"I"] <- (n/S0) * t(x[,i]) %*% MWM %*% x[,i] / crossprod(x[,i],M) %*% x[,i]
     # expected
     out[i,"EI"] <- -1/(n-1)
-    # variance - normality assumption
+    # variance (normality assumption)
     out[i,"VarI"] <- ((n^2*S1 - n*S2 + 3*S0^2) / (S0^2 * (n^2-1))) - out[i,"EI"]^2
     # test statistic
     out[i,"zI"] <- (out[i,"I"]-out[i,"EI"])/sqrt(out[i,"VarI"])
