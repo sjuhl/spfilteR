30fef970bafe27966453c890395e3c2610dd5280
sjuhl <sebastian.juhl@gess.uni-mannheim.de>
2020-10-02 15:27:59 +0200

    improve test coverage

--- tests/testthat/testing.R
+++ tests/testthat/testing.R
@@ -12,6 +12,15 @@ test_that("getEVs() returns a list", {
   expect_is(out, "list")
 })
 
+test_that("getEVs() adds an intercept term", {
+  covars <- fakedataset$x4
+  EVs1 <- getEVs(W=W,covars=covars)$vectors
+  covars2 <- cbind(1,fakedataset$x4)
+  EVs2 <- getEVs(W=W,covars=covars2)$vectors
+  out <- all(EVs1==EVs2)
+  expect_true(out)
+})
+
 
 #####
 # MI.vec()
@@ -33,6 +42,36 @@ test_that("MI.vec() detects NAs and returns an error message", {
   x <- fakedataset$x1
   x[1] <- NA
   expect_error(MI.vec(x=x,W=W), "Missing values detected")
+})
+
+test_that("MI.vec() works with named input", {
+  x <- as.matrix(fakedataset$x1)
+  colnames(x) <- "name"
+  out <- MI.vec(x=x,W=W)
+  expect_equal(rownames(out), colnames(x))
+})
+
+test_that("MI.vec() warns if a constant is supplied", {
+  X <- cbind(1,fakedataset$x1)
+  expect_warning(MI.vec(x=X,W=W), "Constant term detected in x")
+})
+
+test_that("check the permissible attributes for 'alternative'", {
+  x <- fakedataset$x1
+  alternative <- c("greater","lower","two.sided","else")
+  expect <- c(TRUE,TRUE,TRUE,FALSE)
+  out <- NULL
+  for(i in 1:4){
+    res <- try(MI.vec(x=x,W=W,alternative=alternative[i])
+               ,silent=TRUE)
+    out[i] <- class(res)!="try-error"
+  }
+  expect_equal(out, expect)
+})
+
+test_that("W must be of class 'matrix', 'Matrix', or 'data.frame'", {
+  W2 <- as.vector(W)
+  expect_error(MI.vec(x=fakedataset$x1,W=W2), "W must be of class 'matrix' or 'data.frame'")
 })
 
 
@@ -62,6 +101,23 @@ test_that("getMoran() works without supplying x (intercept-only model)", {
   expect_is(out, "data.frame")
 })
 
+test_that("W must be of class 'matrix', 'Matrix', or 'data.frame'", {
+  y <- fakedataset$x1
+  x <- as.matrix(rep(1,length(y)))
+  resid <- y - x %*% solve(crossprod(x)) %*% crossprod(x,y)
+  W2 <- as.vector(W)
+  expect_error(getMoran(resid=resid,W=W2), "W must be of class 'matrix' or 'data.frame'")
+})
+
+test_that("getMoran() warns if boot<100", {
+  y <- fakedataset$x1
+  x <- as.matrix(rep(1,length(y)))
+  resid <- y - x %*% solve(crossprod(x)) %*% crossprod(x,y)
+  boot <- 95
+  test <- tryCatch(getMoran(resid=resid,W=W,boot=boot),warning=function(t) TRUE)
+  expect_true(test)
+})
+
 
 #####
 # partialR2()
@@ -80,6 +136,23 @@ test_that("partialR2() preserves number/names of eigenvectors (if supplied)", {
   colnames(E) <- c("2","4","6")
   out <- partialR2(y=y,x=x,evecs=E)
   expect_equal(names(out), c("2","4","6"))
+})
+
+test_that("partialR2() detects perfect multicollinearity", {
+  y <- fakedataset$x1
+  X <- cbind(1,fakedataset$x2,fakedataset$x2)
+  E <- getEVs(W=W)$vectors[,c(2,4,6)]
+  expect_error(partialR2(y=y,x=X,evecs=E)
+               ,"Perfect multicollinearity in covariates detected")
+})
+
+test_that("partialR2() detects missings", {
+  y <- fakedataset$x1
+  X <- cbind(1,fakedataset$x2)
+  X[1,2] <- NA
+  E <- getEVs(W=W)$vectors[,1:5]
+  expect_error(partialR2(y=y,x=X,evecs=E)
+               ,"Missing values detected")
 })
 
 
@@ -208,6 +281,76 @@ test_that("The argument 'min.reduction' works (for AIC & BIC) - fewer EVs are
   expect_true(all(outAIC,outBIC))
 })
 
+test_that("glmFilter() works with named input", {
+  y <- fakedataset$count
+  x <- as.matrix(fakedataset$x1)
+  colnames(x) <- "name"
+  out <- glmFilter(y=y,x=x,W=W,objfn="p",bonferroni=FALSE
+                   ,model="poisson",sig=.05)
+  expect_equal(rownames(out$estimates)[2], colnames(x))
+})
+
+test_that("glmFilter() breaks if missings are present but na.rm=FALSE", {
+  y <- fakedataset$count
+  y[6] <- NA
+  expect_error(glmFilter(y=y,W=W,model="poisson",na.rm=FALSE)
+               ,"Missing values detected")
+})
+
+test_that("glmFilter() works even if no EVs are selected", {
+  y <- fakedataset$count
+  res <- glmFilter(y=y,W=W,model="poisson",objfn="AIC",min.reduction=.9)
+  expect_equal(res$other$nev,0)
+})
+
+test_that("glmFilter() returns an error message if 'alpha' is not contained
+          in (0,1]", {
+            y <- fakedataset$indicator
+            alpha <- c(-1,.001,.5,1,1.0001)
+            expected <- c(TRUE,FALSE,FALSE,FALSE,TRUE)
+            out <- NULL
+            for(i in 1:length(alpha)){
+              res <- try(glmFilter(y=y,W=W,model="logit",objfn="p",alpha=alpha[i])
+                         ,silent=TRUE)
+              out[i] <- class(res)=="try-error"
+            }
+            expect_equal(out,expected)
+})
+
+test_that("If 'alpha=0', glmFilter() sets 'alpha' to 1e-07", {
+  y <- fakedataset$indicator
+  x <- fakedataset$x3
+  res <- try(glmFilter(y=y,W=W,model="probit",objfn="BIC",alpha=0),silent=TRUE)
+  out <- class(res)!="try-error"
+  expect_true(out)
+})
+
+test_that("glmFilter() detects perfect multicollinearity", {
+  y <- fakedataset$indicator
+  X <- cbind(1,fakedataset$x2,fakedataset$x2)
+  expect_error(glmFilter(y=y,x=X,W=W,model="probit",objfn="p")
+               ,"Perfect multicollinearity in covariates detected")
+})
+
+test_that("glmFilter() works if all EVs should be selected", {
+  y <- fakedataset$count
+  expect_is(glmFilter(y=y,W=W,model="poisson",objfn="all")
+               ,"spfilter")
+})
+
+test_that("if 'objfn=AIC' or 'objfn=BIC', 'min.reduction' needs to be in [0,1)", {
+  y <- fakedataset$indicator
+  reduce <- c(0,.3,1)
+  expect <- c(TRUE,TRUE,FALSE)
+  out <- NULL
+  for(i in 1:3){
+    res <- try(glmFilter(y=y,W=W,model="logit",objfn="AIC",min.reduction=reduce[i])
+               ,silent=TRUE)
+    out[i] <- class(res)!="try-error"
+  }
+  expect_equal(out,expect)
+})
+
 
 #####
 # methods
@@ -235,5 +378,55 @@ test_that("correct dimensionality of vcov()", {
   dim2 <- dim(vcov(out))[2]
   out <- dim1==dim2 & dim1==(ncol(X)+1)
   expect_true(out)
+})
+
+
+#####
+# utils
+#####
+test_that("pfunc() works with empirical p-values and 'alternative=lower'", {
+  z <- .5
+  draws <- rnorm(100,.5,.2)
+  alternative <- c("else","lower")
+  out <-NULL
+  for(i in 1:2){
+    out[i] <- is.numeric(pfunc(z=z,alternative=alternative[i],draws=draws))
+  }
+  expect_true(all(out))
+})
+
+test_that("candsetsize() returns an integer", {
+  zMI <- .5
+  npos <- sum(getEVs(W)$moran>0)
+  out <- candsetsize(npos=npos,zMI=zMI)
+  expect_true(out==round(out))
+})
+
+test_that("residfun() takes as model types 'linear', 'probit'
+          ,'logit', or 'poisson' ", {
+            model <- c("linear","probit","logit","poisson","else")
+            expect <- c(TRUE,TRUE,TRUE,TRUE,FALSE)
+            out <- NULL
+            for(i in 1:5){
+              if(model[i]=="linear"){
+                y <- fakedataset$x3
+                fit <- fakedataset$x3+rnorm(length(y),0,.3)
+              } else if(model[i] %in% c("probit","logit")){
+                y <- fakedataset$indicator
+                fit <- runif(length(y),.001,.999)
+              } else {
+                y <- fakedataset$count
+                fit <- round(fakedataset$x3+rnorm(length(y),0,.3))
+              }
+              res <- try(residfun(y=y,fitvals=fit,model=model[i])
+                         ,silent=TRUE)
+              out[i] <- class(res)!="try-error"
+            }
+            expect_equal(out,expect)
+})
+
+test_that("conditionNumber() returns NULL if no EVs supplied", {
+  out <- conditionNumber(evecs=NULL)
+  expect_null(out)
 })
 
